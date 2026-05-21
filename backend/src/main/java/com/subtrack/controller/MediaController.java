df3ad19b3a93b0b13e28b4c690e91cf71a552c07
Nadir Sidhoum <na.sidhoum@gmail.com>
2026-05-21 02:51:19 +0200

    Added the controller for media (MediaController)

--- backend/src/main/java/com/subtrack/controller/MediaController.java
+++ backend/src/main/java/com/subtrack/controller/MediaController.java
@@ -1,12 +1,43 @@
 package com.subtrack.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.subtrack.entity.MediaType;
+import com.subtrack.service.MediaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/api/media")
 @RequiredArgsConstructor
 public class MediaController {
-  // TODO: wait the implemention of the strategy pattern for different media types (anime, manga, etc.)
+  private final MediaService mediaService;
+
+  @GetMapping("/search")
+  public JsonNode search(
+          @RequestParam MediaType mediaType,
+          @RequestParam String query,
+          @RequestParam int page
+  ) {
+    return mediaService.search(mediaType, query, page);
+  }
+
+  @GetMapping("/{mediaType}/{id}")
+  public JsonNode getById(
+          @PathVariable MediaType mediaType,
+          @PathVariable Long id
+  ) {
+    return mediaService.getById(mediaType, id);
+  }
+
+  @GetMapping("/{mediaType}/{id}/episodes")
+  public JsonNode getEpisodes(
+          @PathVariable MediaType mediaType,
+          @PathVariable Long id
+  ) {
+    return mediaService.getEpisodesById(mediaType, id);
+  }
 }
